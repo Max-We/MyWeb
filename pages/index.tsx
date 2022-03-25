0b7e4d6a221fe14a6636334bc12375c97b6999ba
Max <civnopes@gmail.com>
2022-03-25 14:05:35 +0100

    Changed website name

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,13 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Maximilian Weichart</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Welcome to <span className="text-blue-600">MyWeb!</span>
         </h1>
 
         <p className="mt-3 text-2xl">
