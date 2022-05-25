405342341838fadb5a28250b9a0a7e146d89ed5f
Max <civnopes@gmail.com>
2022-05-25 17:37:33 +0200

    Added thin icon contact me form

--- components/social/twitter/twitter-dm-button.tsx
+++ components/social/twitter/twitter-dm-button.tsx
@@ -9,7 +9,7 @@ export default function TwitterDmButton() {
           className="flex items-center justify-center rounded-full bg-sky-500 p-3 font-bold
         text-white hover:bg-sky-600 dark:bg-sky-500 dark:hover:bg-sky-600 sm:px-10"
         >
-          <ChatIcon className="h-8 w-8 sm:mr-2" />
+          <ChatIcon className="h-8 w-8 stroke-[1.5px] sm:mr-2" />
           <span className="invisible h-0 w-0 sm:visible sm:h-fit sm:w-fit">
             DM me
           </span>
