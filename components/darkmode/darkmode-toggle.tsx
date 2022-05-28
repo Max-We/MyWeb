88c15b2323ec59949d2b9fbb357f6548fbd4e21e
Max <civnopes@gmail.com>
2022-05-28 15:45:27 +0200

    Added darkmode toggle

---
+++ components/darkmode/darkmode-toggle.tsx
@@ -0,0 +1,38 @@
+import { MoonIcon, SunIcon } from '@heroicons/react/outline'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+
+export default function DarkModeToggle() {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) return null
+
+  console.log('theme new new: ' + theme)
+
+  return (
+    <button
+      onClick={changeTheme}
+      className="invisible fixed bottom-0 right-0 mr-5 mb-5 flex h-14 w-14 items-center justify-center rounded-full border bg-white shadow 
+        dark:border-gray-700 dark:bg-gray-800
+        xl:visible"
+    >
+      {theme == 'dark' ? (
+        <SunIcon className="h-8 w-8 stroke-[1.5px]" />
+      ) : (
+        <MoonIcon className="h-8 w-8 stroke-[1.5px]" />
+      )}
+    </button>
+  )
+
+  function changeTheme() {
+    if (theme == 'dark') {
+      setTheme('light')
+    } else {
+      setTheme('dark')
+    }
+  }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar/navbar'
 import { ThemeProvider } from 'next-themes'
-import { useTheme } from 'next-themes'
+import DarkModeToggle from 'components/darkmode/darkmode-toggle'
 
 function MyWeb({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Navbar></Navbar>
+      <DarkModeToggle />
       <div className="container mx-auto py-5 px-3 xl:px-80">
         <Component {...pageProps} />
       </div>
