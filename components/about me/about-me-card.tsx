750fb1422d848db27b6accf641ec4adf1e5f98a3
Max <civnopes@gmail.com>
2022-05-28 16:08:57 +0200

    Added new layout for aboutme page

--- components/about me/about-me-card.tsx
+++ components/about me/about-me-card.tsx
@@ -2,7 +2,7 @@ import { AboutMeCardProps } from './about-me-card.model'
 
 export default function AboutMeCard(props: AboutMeCardProps) {
   return (
-    <div className="mb-7 w-full rounded-xl border p-3 text-center shadow delay-75 duration-100 hover:-translate-y-1 hover:shadow-xl dark:border-gray-700 dark:bg-gray-800 lg:p-5">
+    <div className="w-full rounded-xl border p-3 text-center shadow delay-75 duration-100 hover:-translate-y-1 hover:shadow-xl dark:border-gray-700 dark:bg-gray-800 lg:p-5">
       <h2 className="mb-4 text-2xl">{props.title}</h2>
       <div className="flex flex-col space-y-2">
         {props.content.map((paragraph) => (
